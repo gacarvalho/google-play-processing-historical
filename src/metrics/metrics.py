6081c7d2882f723cf495fe2d259d20b7c53cdae0
Gabriel Carvalho <gacarvalho.contato@gmail.com>
2024-12-26 15:44:50 -0300

    ajuste cols e import

--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -254,8 +254,7 @@ def validate_ingest(spark: SparkSession, df: DataFrame) -> tuple:
     valid_recordsHistcalEmpty = valid_records.filter(cond_histcalEmpty) \
                                  .withColumn("historical_data", empty_array)
 
-    valid_recordsHistcalNotEmpty = valid_records.filter(~cond_histcalEmpty) \
-                                    .withColumn("historical_data", F.col("historical_data").cast(historical_data_schema))
+    valid_recordsHistcalNotEmpty = valid_records.filter(~cond_histcalEmpty)
 
     valid_records = valid_recordsHistcalEmpty.union(valid_recordsHistcalNotEmpty)
 
